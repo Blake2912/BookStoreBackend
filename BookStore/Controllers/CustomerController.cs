b781b1e651a07a8ebdd172f874298e868875070d
Blake2912 <kcvarun29122001@gmail.com>
2023-06-13 16:01:33 +0530

    Data Models Setup and Instatiation of Database Added Migrations for the data models

---
+++ BookStore/Controllers/CustomerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CustomerController : ControllerBase
+	{
+		public CustomerController()
+		{
+		}
+	}
+}
+
